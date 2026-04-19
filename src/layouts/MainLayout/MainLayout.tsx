245ac566e9dfdc58ef981082b05761c43c99d2a0
PhanThanhDat16 <phandat1662004@gmail.com>
2026-04-19 16:08:52 +0700

    feat: dat - setup socket message

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,11 +1,15 @@
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+import FloatingChatBox from '@/components/FloatingChatBox'
 import { Outlet } from 'react-router-dom'
 import { useEffect, useRef, useState } from 'react'
 import { useAuthStore } from '@/store/useAuthStore'
+import { useStoreSocketIO } from '@/store/useStoreSocketIO'
 
 export default function MainLayout() {
   const { accessToken, user, refresh, fetchMe } = useAuthStore()
+  const { connect, disconnect } = useStoreSocketIO()
+
   const [starting, setStarting] = useState(true)
 
   const hasInit = useRef(false)
@@ -29,15 +33,25 @@ export default function MainLayout() {
     run()
   }, [])
 
+  // Initialize socket connection for logged-in users
+  useEffect(() => {
+    connect()
+    return () => {
+      disconnect()
+    }
+  }, [])
+
   if (starting) {
-    return <div className="flex h-screen items-center justify-center">Đang tải trang...</div>
+    return <div className="flex items-center justify-center h-screen">Đang tải trang...</div>
   }
+
   return (
     <>
       <Header />
       <main className="min-h-screen bg-white">
         <Outlet />
       </main>
+      <FloatingChatBox />
       <Footer />
     </>
   )
